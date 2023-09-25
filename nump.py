bd1535cd14a6edfde9577b43ac06fb66f600f3b6
AlexEnchanter <43272438+AlexEnchanter@users.noreply.github.com>
2023-09-25 22:02:56 +0200

    Update nump.py

--- nump.py
+++ nump.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,22 +9,26 @@ names = ["web serch ate20 atc20",  "data mining ate20 atc20",
          "web serch ate80 atc20",  "data mining ate80 atc20",
          "web serch ate80 atc160", "data mining ate80 atc160"]
 
-# for i in range(len(files)):
 one2ten = np.arange(1, 11, 1)
-data = np.load(files[0], allow_pickle=True)
-mean = [np.mean(data[i], axis=0) for i in range(10)]
-percentile_95 = [np.percentile(data[i], 95, axis=0) for i in range(10)]
-percentile_99 = [np.percentile(data[i], 99, axis=0) for i in range(10)]
-plt.plot(one2ten, mean, label="mean")
-plt.plot(one2ten, percentile_95, label="95%")
-plt.plot(one2ten, percentile_99, label="99%") 
-plt.xlabel("flows/s")
-plt.ylabel("Completion Time (s)")
-plt.xticks(np.arange(1, 11, 1))
-plt.title(names[0])
-# plt.yscale("log")
-plt.legend()
-plt.savefig(f"{names[0]}.png")
-plt.clf()
+for i in range(len(files)): 
+    if not os.path.exists(files[i]):
+        continue
 
+    data = np.load(files[i], allow_pickle=True)
+    mean = [np.mean(data[i], axis=0) for i in range(10)]
+    std = [np.std(data[i], axis=0) for i in range(10)]
+    percentile_95 = [np.percentile(data[i], 95, axis=0) for i in range(10)]
+    percentile_99 = [np.percentile(data[i], 99, axis=0) for i in range(10)]
+    plt.errorbar(one2ten, mean, label="mean", yerr = std, capsize = 4)
+    plt.errorbar(one2ten, percentile_95, label="95%", yerr = std, capsize = 4)
+    plt.errorbar(one2ten, percentile_99, label="99%", yerr = std, capsize = 4) 
+    plt.xlabel("flows/s")
+    plt.ylabel("Completion Time (s)")
+    plt.xticks(np.arange(1, 11, 1))
+    plt.title(names[i])
+    plt.legend()
+    if "data" in names[i]:
+        plt.yscale("log") 
+    plt.savefig(f"{names[i]}.png")
+    plt.clf()
 
