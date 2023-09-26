b899815f8fb67b281e0bbc6bb50dba2ba9dc4d37
AlexEnchanter <43272438+AlexEnchanter@users.noreply.github.com>
2023-09-26 21:35:25 +0200

    argparse

--- topiperf.py
+++ topiperf.py
@@ -3,6 +3,7 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.net import Mininet
 from mininet.log import setLogLevel
+import argparse
 import math 
 import numpy as np
 import random
@@ -15,14 +16,13 @@ x_web = [0,10_000, 20_000,30_000,50_000,80_000,200_000, 1_000_000, 2_000_000,5_0
 y_web = [0., 0.18, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.98, 1.]
 x_data = [0,180,216,560,900,1_100,1_870, 3_160, 10_000, 400_000, 3_160_000, 100_000_000, 1_000_000_000]
 y_data = [0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ,0.97, 0.99, 1.]
-
 edge_to_host_bw = 20
-agr_to_core_bw = 20
+agr_to_core_bw = 20 
 agr_to_edge_bw = 20
 
 debug = False
 
-class MyTopo( Topo  ):
+class MyTopo(Topo):
     def build( self ):
         aggr_layer_switches = 4
         edge_layer_switches = 8
@@ -46,7 +46,7 @@ class MyTopo( Topo  ):
 #source = source, sink = destination, _type [1 = web search, 2 = data mining], intensity = flows/s, gen_time = duration 
 traffic_types = {1:{'name':'web search', 'flow':80000},2:{'name':'data mining', 'flow':1100}} 
 
-def evaluation(_type): 
+def evaluation(_type, gen_time):  
     print(f'doing: data_{traffic_types[_type]["name"]}_ate-{agr_to_edge_bw}_atc-{agr_to_core_bw}')
     count = 0
     result = [[]]*10 
@@ -54,55 +54,50 @@ def evaluation(_type):
         for i in range(1,11):
             t1, t2 = getRandomHosts()
             before_time = time.time() 
-            flow_time_list, total_data = genIperf(f'h{t1}',f'h{t2}', _type, i, 10)
+            flow_time_list, total_data = genIperf(f'h{t1}',f'h{t2}', _type, i, gen_time)
             after_time = time.time()  
             result[i-1] = result[i-1] + flow_time_list
             count += 1
             print(f" count: {count}, avg time: {round(np.mean(flow_time_list), 4)}; min {round(min(flow_time_list), 4)}; max {round(max(flow_time_list), 4)}, total data: {round(total_data, 4)} MB, real time: {round(after_time - before_time, 4)}\n")
-            print(f" count: {count}, avg time: {round(np.mean(flow_time_list), 4)}; min {round(min(flow_time_list), 4)}; max {round(max(flow_time_list), 4)}, total data: {round(total_data, 4)} MB, real time: {round(after_time - before_time, 4)}\n", file=sys.stderr)
     np_list = np.array(result, dtype=object)
     np.save(f'data_{traffic_types[_type]["name"]}_ate-{agr_to_edge_bw}_atc-{agr_to_core_bw}', np_list)	
 
 def genIperf(source = None, sink = None, _type =None, intensity = None, gen_time = None):
-    gen_func = None
-    if _type == 1:
-        gen_func = webCDF
-    else: 
-        gen_func = dataCDF
+    gen_func = webCDF if _type == 1 else dataCDF
     
     print(f"{source} -> {sink}", file=sys.stderr)
     source_node = net.get(source)
-    source_ip = source_node.IP()
     sink_node = net.get(sink)
     sink_ip = sink_node.IP()
     num_of_flows = gen_time*intensity
     
     subprocess.getoutput("rm ./iperflog.txt")
-    print(f"total flows: {num_of_flows}")
-    print(f"total flows: {num_of_flows}", file=sys.stderr)
-    print(f'running command: iperf -s -P {num_of_flows} -f m -o iperflog.txt &', file=sys.stderr)
+    if(debug):
+    	print(f"total flows: {num_of_flows}")
+    	
+    print(f'running command: iperf -s -P {num_of_flows} -f m -o iperflog.txt &')
     sink_node.cmd(f'iperf -s -P {num_of_flows} -f m -o iperflog.txt &') 
     iperfcmd = "iperf -c {} -n {}K &"
 
-    aaa = 0
+    total_flow_size = 0
     for i in range(gen_time):
         for k in range(intensity):
-            data = gen_func(_type)
-            aaa += data
-            print(f"running command: {iperfcmd.format(sink_ip, data)}", file=sys.stderr)
-            source_node.cmd(iperfcmd.format(sink_ip, data))
+            flow_size = gen_func(_type)
+            total_flow_size += flow_size
+            print(f"running command: {iperfcmd.format(sink_ip, flow_size)}")
+            source_node.cmd(iperfcmd.format(sink_ip, flow_size))
         time.sleep(1) # wait 1s 
 
-    print(f"waiting for senders to finish, total data to send:{aaa/1000}Mb over {num_of_flows} flows")
-    print(f"waiting for senders to finish, total data to send:{aaa/1000}Mb over {num_of_flows} flows", file=sys.stderr)
+    if(debug):
+    	print(f"waiting for senders to finish, total data to send:{total_flow_size/1000}Mb over {num_of_flows} flows")
     sink_node.cmd("wait") 
-    print("done waiting", file=sys.stderr)
 
     iperf_out = subprocess.getoutput("grep -v 'SUM' iperflog.txt | grep -e '[1-9]] [0-9]'")
     times = []
     data_sent = 0
     for lines in iperf_out.split('\n'):
         times.append(float(re.findall("[0-9]+\.?[0-9]*", lines)[2]))
+        
         temp       = float(re.findall("[0-9]+\.?[0-9]*", lines)[3])
         if re.search("Kbytes", lines):
             data_sent += temp/1000
@@ -112,73 +107,48 @@ def genIperf(source = None, sink = None, _type =None, intensity = None, gen_time
             data_sent += temp
     return times, data_sent             
 
-
+#The different types of distributions from normal data traffic of a data center network flow, returns a flow size
 def dataCDF(_):
-    return math.ceil(np.interp(random.random(),y_data,x_data)/1000)
+	return math.ceil(np.interp(random.random(),y_data,x_data)/1000)
 	
 def webCDF(_):
 	return math.ceil(np.interp(random.random(),y_web,x_web)/1000)
-	
+		
 def expCDF(_type):
 	mean_flow = traffic_types[_type]['flow']
 	return math.ceil(np.random.default_rng().exponential(mean_flow)/1000)
 
+
 def getRandomHosts():
-	t1 = random.randint(1,16)
-	while True:
-		t2 = random.randint(1,16)
-		if not t1 == t2:
-			break 
-	return t1, t2
-
-setLogLevel('info')
-
-edge_to_host_bw = 20
-agr_to_core_bw = 20
-agr_to_edge_bw = 20 
-
-# data 20
-topos = MyTopo()
-net = Mininet(topos, link=TCLink)
-net.start()
-evaluation(2)
-net.stop()
- 
-# web 20
-topos = MyTopo()
-net = Mininet(topos, link=TCLink)
-net.start()
-evaluation(1)
-net.stop()
- 
-# agr_to_edge_bw = 80 
-# # data 80
-# topos = MyTopo()
-# net = Mininet(topos, link=TCLink)
-# net.start()
-# evaluation(2)
-# net.stop()
- 
-# # web 80
-# topos = MyTopo()
-# net = Mininet(topos, link=TCLink)
-# net.start()
-# evaluation(1)
-# net.stop()
+	return random.sample(list(range(1,17)), 2)
 
 
-# agr_to_core_bw = 160
-# agr_to_edge_bw = 80 
-# # data 80 anb 160
-# topos = MyTopo()
-# net = Mininet(topos, link=TCLink)
-# net.start()
-# evaluation(2)
-# net.stop()
- 
-# # web 80 anb 160
-# topos = MyTopo()
-# net = Mininet(topos, link=TCLink)
-# net.start()
-# evaluation(1)
-# net.stop()
+parser = argparse.ArgumentParser(description='Generates and executes data center traffic on mininet.')
+parser.add_argument('-e2h_bw',nargs = '*', help ='Sets the bandwidth between the edge layer switches and hosts', type = int )
+parser.add_argument('-a2e_bw',nargs = '*', help ='Sets the bandwidth between the aggregation layer switches and the edge layer switches', type = int )
+parser.add_argument('-c2a_bw',nargs = '*', help ='Sets the bandwidth between the aggregation layer switches and the core layer switch', type = int )
+parser.add_argument('-d',action = 'store_true', help ='Sets debug mode active')
+parser.add_argument('-t', nargs = '?', type = int, default = 10, help ='Sets the generation time, default time is 10s')
+args = parser.parse_args()
+
+
+
+if(args.d):
+	print(args)
+	debug = True
+	setLogLevel('info')
+	
+if(not len(args.e2h_bw) == len(args.a2e_bw) == len(args.c2a_bw)):
+	print('Bandwidth arguments must be of equal length')
+	exit()
+	
+for i in range(len(args.e2h_bw)):
+	edge_to_host_bw = args.e2h_bw[i]
+	agr_to_core_bw = args.a2e_bw[i]
+	agr_to_edge_bw = args.c2a_bw[i]
+	topos = MyTopo()
+	net = Mininet(topos, link=TCLink)
+	net.start()
+	evaluation(1,args.t)
+	evaluation(2,args.t)
+	net.stop()  
